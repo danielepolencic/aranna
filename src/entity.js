16939b83c5bbac422f0ab2874512ac437c349204
Daniele Polencic <daniele.polencic@gmail.com>
2014-06-26 09:31:53 +0100

    - refactored entity class to take advantage of add and remove component events - listeners are currently saved as singletons

--- src/entity.js
+++ src/entity.js
@@ -1,17 +1,63 @@
-module.exports = Entity;
+var util = require('./util')
+  , observer = require('./observer')
+  , publisher = {}
+  , subscriber = {}
+  , unsubscriber = {}
+  , components = new Map();
 
-function Entity () {
-  Map.call(this);
-}
-
-Entity.prototype = Object.create(Map.prototype, {
-  constructor: {value: Entity}
+['componentAdded', 'componentRemoved'].forEach(function (event) {
+  var listeners = new Map();
+  [
+    {name: 'publisher', variable: publisher},
+    {name: 'unsubscriber', variable: unsubscriber},
+    {name: 'subscriber', variable: subscriber}
+  ]
+  .forEach(function (item) {
+    item.variable[event] = observer[item.name](listeners);
+  });
 });
 
-Entity.prototype.addComponent = function (component) {
-  return this.set(component.name, component);
+module.exports = Entity;
+
+function Entity () {}
+
+Entity.prototype.addComponent = util.pipeline(
+  addComponentTo(components),
+  reduceComponentsToTopics(components),
+  util.spread(publisher.componentAdded.publish)
+);
+
+Entity.prototype.removeComponent = util.pipeline(
+  removeComponentFrom(components),
+  reduceComponentsToTopics(components),
+  util.spread(publisher.componentRemoved.publish)
+);
+
+Entity.prototype.onComponentAdded = subscriber.componentAdded.subscribe;
+Entity.prototype.onComponentRemoved = subscriber.componentRemoved.subscribe;
+
+Entity.prototype.offComponentAdded = unsubscriber.componentAdded.unsubscribe;
+Entity.prototype.offComponentRemoved = unsubscriber.componentRemoved.unsubscribe;
+
+function addComponentTo (components) {
+  return function (component) {
+    components.set(component.name, component);
+    return component;
+  }
 };
 
-Entity.prototype.removeComponent = function (component) {
-  return this.remove(util.isString(component) ? component : component.name);
+function removeComponentFrom (components) {
+  return function (component) {
+    return components.delete(util.isString(component) ? component : component.name);
+  }
+};
+
+function reduceComponentsToTopics (components) {
+  return function (component) {
+    var topics = [];
+    components.forEach(function (componentValue, componentName) {
+      topics.push(componentName);
+    });
+    return [topics, [component, this]];
+  };
 };
--- test/entity.js
+++ test/entity.js
@@ -1,1 +1,51 @@
+var assert = require('assert');
+
 require('es6-shim');
+
+var Entity = require('./../src/entity');
+
+describe.only('Entity', function () {
+  var entity;
+
+  beforeEach(function () {
+    entity = new Entity();
+  });
+
+  it('should add a component', function (done) {
+    var component = {name: 'position'};
+    entity.onComponentAdded('position')
+    .listener(function (componentClone, entityClone) {
+      assert.deepEqual(entity, entityClone);
+      assert.deepEqual(component, componentClone);
+      done();
+    });
+    entity.addComponent(component);
+  });
+
+  it('should remove a component', function (done) {
+    var component = {name: 'position'};
+    entity.onComponentRemoved('position')
+    .listener(function (componentClone, entityClone) {
+      assert.deepEqual(entity, entityClone);
+      assert.deepEqual(component, componentClone);
+      done();
+    });
+    entity.addComponent(component);
+    entity.removeComponent(component);
+  });
+
+  it('should not propagate any update if listeners are removed', function () {
+    var component = {name: 'position'};
+    var listener = function () {
+      done('should not have been called');
+    };
+    entity.onComponentAdded('position').listener(listener);
+    entity.onComponentRemoved('position').listener(listener);
+    entity.offComponentAdded('position').listener(listener);
+    entity.offComponentRemoved('position').listener(listener);
+    entity.addComponent(component);
+    entity.removeComponent(component);
+    done();
+  });
+
+});
